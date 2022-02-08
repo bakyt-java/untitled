27bd15e98e618c30f18a8c77c0aaebc2a8282fad
Hello World app <bakyt.ikramov@iaau.edu.kg>
2022-02-08 17:43:18 +0600

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat("Bekzat",12,"black");
         System.out.println("cat name : " + cat.name + "\ncat age :"+cat.age + "\ncat color :" + cat.color);
+        System.out.println("cat name : " + cat.name + "\ncat age :"+cat.age + "\ncat color :" + cat.color);
     }
 }
